04136dce92b93064ac3a9fd6ddb721eeebd2d4ef
kalene APIOU <apiou.kalene@gmail.com>
2020-02-03 22:29:58 +0100

    Ajout de Turtle.py

---
+++ Turtle.py
@@ -0,0 +1,15 @@
+import math
+from math import *
+from turtle import *
+
+
+# forward(120)
+# left(90)
+# color('red')
+# forward(80)
+# reset()
+a = 0
+while a < 12:
+    a = a + 1
+    forward(150)
+    left(150)
