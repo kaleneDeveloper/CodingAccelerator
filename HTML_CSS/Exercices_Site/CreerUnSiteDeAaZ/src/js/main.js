befcb88addecc45c415f27bf518d86610e72bbf6
kalene APIOU <apiou.kalene@gmail.com>
2020-02-28 15:37:45 +0100

    Update

--- HTML_CSS/Exercices_Site/CreerUnSiteDeAaZ/src/js/main.js
+++ HTML_CSS/Exercices_Site/CreerUnSiteDeAaZ/src/js/main.js
@@ -60,3 +60,10 @@ $(document).ready(function (){
 		});
 	});
 });
+
+let btn = document.querySelector('.toggle_btn');
+let nav = document.querySelector('.nav');
+
+btn.onclick = function() {
+    nav.classList.toggle('nav_open');
+}
